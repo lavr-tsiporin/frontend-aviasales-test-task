de3a1985f8e579855e063b87ae6b4df7bd344763
Lavr <avatar_str@list.ru>
2022-06-27 21:13:10 +0500

    feat: create button load more tickets

---
+++ src/components/ButtonDowloadMore/ButtonDownloadMore.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ButtonDowloadMore.css";
+
+export const ButtonDownloadMore = () => {
+  return (
+    <button className="btn btn-load-more text-upper">
+      Показать еще 5 билетов!
+    </button>
+  );
+};
